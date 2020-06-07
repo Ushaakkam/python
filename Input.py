446b13a2257bacdc24749f274f380fe2c4602e02
Ushaakkam <66548901+Ushaakkam@users.noreply.github.com>
2020-06-07 14:27:20 +0530

    input function added

---
+++ Input.py
@@ -0,0 +1,7 @@
+
+
+ x=int(input("enter the value x value:"))
+ y=int(input("enter the value y value:"))
+ z=int(input("enter the value z value:"))
+ print(max(x,y,z))
+input("please press enter to exit")
