365f68f0f511f26f4fd657932b14a0ffd84f6d8f
Ushaakkam <66548901+Ushaakkam@users.noreply.github.com>
2020-06-07 14:26:10 +0530

    arthmetic operations added

---
+++ Arithmetic.py
@@ -0,0 +1,28 @@
+#addition
+v1=int(input("enter the value 1:"))
+v2=int(input("enter the value 2:"))
+print(v1+v2)
+
+#subtraction
+v3=int(input("enter the value 1:"))
+v4=int(input("enter the value 2:"))
+print(v3-v4)
+
+#multiplication
+v5=int(input("enter the value 1:"))
+v6=int(input("enter the value 2:"))
+print(v5*v6)
+
+#division (quotient based value)
+
+v7=int(input("enter the value 1:"))
+v8=int(input("enter the value 2:"))
+print(v7/v8)
+
+#modulo(remainder based value)
+v9=int(input("enter the value 1:"))
+v10=int(input("enter the value 2:"))
+print(v9%v10)
+
+input("please press enter if you want to exit from the screen")
+
