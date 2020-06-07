356e006699d01f976f00a904214c8936cbcae3bd
Ushaakkam <66548901+Ushaakkam@users.noreply.github.com>
2020-06-07 15:47:25 +0530

    intro for list

---
+++ list.py
@@ -0,0 +1,39 @@
+animals=["dog","cat","elephant","lion"]
+print(animals)
+
+list1=[20,30,40,'usha',30.5,'S','aswi']
+print(list1)
+
+list2=['usha','aswi','suni','subbu']
+print(list2)
+
+list3=['gangaiah','naga lakshmi','changal rayudu','saila','varsha']
+print(list3)
+
+list4=[[10,3],[20,4]]
+print(list4)
+
+
+#list operations
+''' REPLACE,APPEND,SORT,DELETE,INSERT,REVERSE,REMOVE,EXTEND '''
+list1[1]=10
+print(list1)
+
+
+list3.append('hanith')
+print(list3)
+
+''' list2.sort(list2)
+list2 '''
+
+del list4[1]
+
+list4.insert(1,'london')
+
+list1.reverse()
+
+list1.remove(20)
+
+list2.extend(list3) #we can add one list into another list
+
+
